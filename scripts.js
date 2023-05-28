ea8ee89833444e46a792d92d96c0077c18aba914
LuuGolis <golischevskyl@gmail.com>
2023-05-28 15:22:42 -0300

    Lógica JS: Calculo totalPago

--- scripts.js
+++ scripts.js
@@ -1,1 +1,31 @@
-console.log("hola desde js");
+
+function mostrarCategoria(){
+let selectedValue = document.getElementById("categoria").value;
+console.log(selectedValue);
+
+}
+
+function tipoDescuento(categoria){
+    let descuento = 0;
+    
+    if(categoria === 'estudiante'){
+        descuento = 80;
+    }
+    if(categoria === 'trainee'){
+        descuento = 50;
+    }
+    if(categoria === 'junior'){
+        descuento = 15;
+    }
+    return descuento;
+}
+
+function totalAPagar(cantidad, descuento){
+    let total = cantidad * 200;
+    let totalDescuento = (total * descuento) / 100;
+    let totalPago = total - totalDescuento;
+return totalPago;
+}
+
+let prueba = totalAPagar(5,15);
+console.log(prueba);
